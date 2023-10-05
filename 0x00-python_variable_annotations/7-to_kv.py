c835b3fbb769e14f12407a65674fffa54b9e1a10
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 15:54:07 +0300

    add doc

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+'''accept string and int or float returns tuple'''
 from typing import Union, Tuple
 
 def to_kv(k: str,v: Union[int, float]) -> Tuple:
+    '''return tuple of string and square of int or float'''
     pass
