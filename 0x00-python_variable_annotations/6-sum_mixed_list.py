878e1bde91d03f783a26c42b6183860f579b6375
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 15:45:42 +0300

    remove |

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Return sum of list of floats """
     if mxd_lst is None:
         return 0
