5cd8b1ba8a6e051c46736a9afd7fc516586b0220
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 16:27:53 +0300

    add doc

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+'''
+type annotated function safe_first_element that takes a list
+input_list ofany type elements and returns the first element
+'''
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''returns first element of list'''
     if lst:
         return lst[0]
     else:
