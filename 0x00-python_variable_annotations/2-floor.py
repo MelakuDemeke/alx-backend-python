41451b42a098c2b15e4925a033c58061fa229ee2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 12:02:17 +0300

    takes float and return floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+return floor of float
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    takes a float n as argument and returns the floor of the float.
+    '''
+    return int(n)
