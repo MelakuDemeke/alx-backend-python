ddc5e585de9b475b0e00623a7f961d1b7af9cbe7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 16:41:01 +0300

    fix according to requrement

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""get value from dictionary"""
+from typing import TypeVar, Mapping, Any, Optional, Union
+
+T = TypeVar('T')
 
 
-def safely_get_value(dct, key, default = None):
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
+    """Return value linked to key in dictionary or default.
+    """
     if key in dct:
         return dct[key]
     else:
