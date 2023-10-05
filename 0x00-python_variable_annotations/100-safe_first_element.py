bfe729e6ada125eb6d6ab844a9974e6d82e7a61e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 16:27:03 +0300

    type annoted

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst):
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
