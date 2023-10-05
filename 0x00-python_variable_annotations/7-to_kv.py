190534396101ae59778e4210ec88e3fc48b4d822
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 15:55:21 +0300

    pycode style fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 '''accept string and int or float returns tuple'''
 from typing import Union, Tuple
 
-def to_kv(k: str,v: Union[int, float]) -> Tuple:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
     '''return tuple of string and square of int or float'''
-    return(k, v**2)
+    return (k, v**2)
