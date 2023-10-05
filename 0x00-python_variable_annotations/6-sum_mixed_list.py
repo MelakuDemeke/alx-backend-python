5a4ae0334fcbafa737ec474c54222a69ed9dc67f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 15:40:41 +0300

    import list from typing

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+'''return sum of mixed list of floats and ints'''
+from typing import List
 
-def sum_mixed_list(mxd_lst: [int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
     pass
