2078a7d4ee72a9bb6d9aa90caf11454b90e65cc9
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-05 14:57:52 +0300

    doc added

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+''' return string representation of float '''
+
 
 def to_str(n: float) -> str:
+    '''
+    accept folating number and return string representation of it
+    '''
     return str(n)
