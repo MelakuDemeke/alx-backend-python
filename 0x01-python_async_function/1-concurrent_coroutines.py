9749fd7d085c1ba4dcf8d47daea5a6740ff656d3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-09 11:52:09 +0300

    type anotiate the function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 import random
+from typing import List
 '''
 wait for random time and return the delay time in list
 '''
@@ -8,7 +9,7 @@ wait for random time and return the delay time in list
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     Spawn wait_random n times with the specified max_delay.
     Return the list of all the delays (float values) in ascending order.
