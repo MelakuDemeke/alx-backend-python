a52ff2458f4c935a73dfc2df80d958f65537a44e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-09 11:29:06 +0300

    add doc

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-
+'''wait for random time and return the delay time'''
 import asyncio
 import random
 
 async def wait_random(max_delay=10):
+    '''
+    wait for random seconds and return the delay time
+    '''
     delaytime = random.uniform(1, max_delay)
     await asyncio.sleep(delaytime)
     return delaytime
