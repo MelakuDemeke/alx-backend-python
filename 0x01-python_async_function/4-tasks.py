e23c1edab669067594990f4d3dff79b42a91c4f8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-09 12:51:48 +0300

    improve doc

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-wait for random time and return the delay time in list
+Wait for random durations and return the delay times in a list.
 '''
 import asyncio
 import random
@@ -13,8 +13,14 @@ tak_3 = __import__('3-tasks').wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''
-    Spawn wait_random n times with the specified max_delay.
-    Return the list of all the delays (float values) in ascending order.
+    Create multiple tasks that wait for random durations with max delay.
+
+    Args:
+        n (int): The number of times to create tasks.
+        max_delay (int): The maximum delay time (in milliseconds) for each task
+
+    Returns:
+        List[float]: A list of delay times (float values) in ascending order.
     '''
     tasks = [tak_3(max_delay) for _ in range(n)]
     delays = [await task for task in asyncio.as_completed(tasks)]
