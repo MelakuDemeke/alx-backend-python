2ff8c9440f79e9466f28c8fc4daac851f254e6cd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-10 11:04:20 +0300

    add proper doc

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+'''
+This script imports an asynchronous generator from another module
+and defines an asynchronous comprehension to collect its values.
+'''
 import asyncio
 asyngen = __import__('0-async_generator').async_generator
 
 async def async_comprehension():
+    '''
+    Asynchronous comprehension to collect values from an async generator.
+
+    Returns:
+        list: A list containing the values yielded by the async generator.
+
+    Usage:
+        result = await async_comprehension()
+    '''
     return [i async for i in asyngen()]
