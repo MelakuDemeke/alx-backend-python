7196222da9fea897c3eab8bae77a4f8587b99ef3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-10 11:11:28 +0300

    run the asynccomp 4 tiems and caluculate execution time

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,4 +4,9 @@ asynccomp = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime():
-    pass
+    start_time = time.time()
+    tasks = [asynccomp() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return execution_time
