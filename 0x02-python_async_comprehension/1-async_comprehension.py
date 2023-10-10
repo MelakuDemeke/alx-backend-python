a64b6d0feb330becee33e22a6b2927763ee49d25
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-10 11:02:31 +0300

    change import file

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import asyncio
-asyngen = __import__('1-async_comprehension').async_generator
+asyngen = __import__('0-async_generator').async_generator
 
 async def async_comprehension():
     return [i async for i in asyngen()]
