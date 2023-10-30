22d4216c7c00d057acd5dab8dc4f6448862a86d6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-30 11:22:00 +0300

    def logic and import lib

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,19 @@
 """Module for testing the Client class."""
 
 import unittest
+from typing import Dict
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+
 
 class TestGithubOrgClient(unittest.TestCase):
-    pass
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
+        pass
 
