6aba2eac4d50565330e38dd169861b59bb10dc8c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-30 12:22:49 +0300

    start get_patcher

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -160,3 +160,6 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             if url in route_payload:
                 return Mock(**{'json.return_value': route_payload[url]})
             return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
