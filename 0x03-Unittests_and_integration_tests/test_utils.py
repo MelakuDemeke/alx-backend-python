17bfd90c7cbac30fa892e76774ecc10a114ccb69
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-30 10:43:45 +0300

    add parametrized expand and def the func

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -42,3 +42,10 @@ class TestAccessNestedMap(unittest.TestCase):
         - None
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, exception):
+        pass
