6bd402703685b98ebf32f061452ea6c14bfb18fc
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-30 10:38:17 +0300

    add doc

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,25 @@ as expected and do not introduce any regressions.
 """
 
 import unittest
+from parameterized import parameterized
+from utils import access_nested_map
 
-class TestAccessNestedMap(unittest.TestCase):
+
+def access_nested_map(nested_map, path):
+    """
+    Accesses a nested value in a dictionary using a sequence of keys.
+
+    Args:
+        nested_map (dict): A dictionary with nested values.
+        path (tuple): A sequence of keys to access the nested value.
+
+    Returns:
+        The value at the end of the path.
+
+    Raises:
+        KeyError: If any key in the path is not found in the dictionary.
+        TypeError: If the argument is not a dictionary or the path is not a tuple.
+    """
+
     pass
 
