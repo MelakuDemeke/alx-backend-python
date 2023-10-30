92f52b4d34b029846e4573b467e24146305bf16a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-30 10:32:17 +0300

    begin and add doc string

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module contains unit tests for the utils module.
+
+The utils module provides various utility functions that are used throughout
+the project.These tests ensure that the functions in the utils module behave
+as expected and do not introduce any regressions.
+"""
+
+
