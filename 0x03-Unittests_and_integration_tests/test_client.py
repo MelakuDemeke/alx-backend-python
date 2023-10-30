8a918bb72a907697eb288dc1ab97a9c7e3b686e8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-30 12:25:09 +0300

    implement test public repos with license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -174,3 +174,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
                 GithubOrgClient("google").public_repos(),
                 self.expected_repos,
             )
+
+    def test_public_repos_with_license(self) -> None:
+            """
+            Test that the public_repos method of the GithubOrgClient
+            class returns the expected list of repositories
+            with the specified license for a given organization.
+            """
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(license="apache-2.0"),
+                self.apache2_repos,
+            )
